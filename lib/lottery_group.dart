8c6f49ca1a9bfbebe51f991b087017d789a128e1
zhangyuan <yuan.zhang@hipac.cn>
2023-03-14 23:51:34 +0800

    ui

--- lib/lottery_group.dart
+++ lib/lottery_group.dart
@@ -7,18 +7,21 @@ import 'lottery_item.dart';
 class LotteryGroupView extends StatelessWidget {
   final List<Lottery> lotteryList;
 
-  const LotteryGroupView(this.lotteryList,{super.key });
+  const LotteryGroupView(this.lotteryList, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: BallColors.bg_content,
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: lotteryList.map((e) => LotteryItemView( e)).toList(),
-        ),
-      ),
+      child: ListView.separated(
+          padding: const EdgeInsets.all(12),
+          itemBuilder: (context, index) {
+            return LotteryItemView(lotteryList[index]);
+          },
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 12);
+          },
+          itemCount: lotteryList.length),
     );
   }
 }
